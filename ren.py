53a8e24488281eb58e9273d5d35fb8bef6859d28
renjacbernadez2-ui <renjacbernadez2@gmail.com>
2025-11-12 16:26:55 +0800

    Update ren.py

--- ren.py
+++ ren.py
@@ -1,1 +1,15 @@
+# Simple Login System in Python
 
+# Step 1: Store username and password
+USERNAME = "admin"
+PASSWORD = "12345"
+
+# Step 2: Ask user for input
+entered_username = input("Enter username: ")
+entered_password = input("Enter password: ")
+
+# Step 3: Check credentials
+if entered_username == USERNAME and entered_password == PASSWORD:
+    print(" Login successful! Welcome,", USERNAME) 
+else:
+    print(" Invalid username or password. Try again.") 
